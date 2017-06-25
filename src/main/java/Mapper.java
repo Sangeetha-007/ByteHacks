8c2ac14feb2249d3b9ed5bd09ecf0472a0268ce8
Akhila Ananth <aananth@Akhilas-MacBook-Pro.local>
2017-06-25 07:22:01 -0400

    Fetches information from wikipedia

---
+++ src/main/java/Mapper.java
@@ -0,0 +1,20 @@
+import java.io.*;
+
+
+/**
+ * Created by aananth on 6/25/17.
+ */
+public class Mapper {
+
+
+    public Mapper(){
+    }
+
+
+
+
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ src/main/java/TreasuresController.java
@@ -0,0 +1,19 @@
+/**
+ * Created by aananth on 6/25/17.
+ */
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class TreasuresController {
+
+
+
+    @GetMapping("/treasure")
+    public String sendBlurb(){
+        WikipediaClient client = new WikipediaClient();
+        String result = client.getContentForSearchTerm("Ada Lovelace");
+    }
+}
