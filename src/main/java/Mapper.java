c05e83b8a8e29cdbe08bbfafe7d74ddcbc7c760a
Akhila Ananth <aananth@Akhilas-MacBook-Pro.local>
2017-06-25 07:32:28 -0400

    added mapper interface

--- src/main/java/Mapper.java
+++ src/main/java/Mapper.java
@@ -4,14 +4,16 @@ import java.io.*;
 /**
  * Created by aananth on 6/25/17.
  */
-public class Mapper {
+public class Mapper implements IMapper{
 
 
     public Mapper(){
     }
 
 
+    public String getTreasure(){
 
+    }
 
     public static void main(String[] args) {
 
--- src/main/java/TreasuresController.java
+++ src/main/java/TreasuresController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 @Controller
 public class TreasuresController {
 
-
+    Mapper map = new Mapper();
 
     @GetMapping("/treasure")
     public String sendBlurb(){
