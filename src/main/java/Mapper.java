ea23df52bc847b926d1b6454bd828d5537171d06
Juanita563 <jlanderson563@gmail.com>
2017-06-25 11:17:16 -0400

    added mapper class

--- src/main/java/Mapper.java
+++ src/main/java/Mapper.java
@@ -1,18 +1,20 @@
 import java.io.*;
+import java.util.ArrayList;
+import java.util.Random;
+
 
 
 /**
  * Created by aananth on 6/25/17.
  */
 public class Mapper implements IMapper{
-
-
-    public Mapper(){
-    }
-
+    private String[] women = {"Grace Hopper", "Evelyn Boyd Granville", "Ida Rhodes", "Mary Combs", "Dana Ulery", "Jean E. Sammet", "Dana Ulery",
+    "Jean E. Sammet", "Dame Stephanie \"Steve\" Shirley", "Mary Allen Wilkes", "Sister Mary Kenneth Keller", "Margaret R. Fox", "Margaret Hamilton"};
 
     public String getTreasure(){
-        return "";
+        Random rand = new Random();
+        int  n = rand.nextInt(55);
+        return this.women[n];
     }
 
     public static void main(String[] args) {
